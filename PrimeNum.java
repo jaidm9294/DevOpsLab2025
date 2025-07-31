fe246604c2b6ab04b10eb0c654e108e4c832190e
jaidm9294 <mullajaid92111@gmail.com>
2025-07-31 14:45:28 +0530

    This is my Prime Number code in java

---
+++ PrimeNum.java
@@ -0,0 +1,23 @@
+public class PrimeCheck {
+    public static void main(String[] args) {
+        int number = 29; // You can change this to any number you want to test
+
+        if (isPrime(number)) {
+            System.out.println(number + " is a Prime number.");
+        } else {
+            System.out.println(number + " is NOT a Prime number.");
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        if (n == 2 || n == 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) return false;
+        }
+
+        return true;
+    }
+}
